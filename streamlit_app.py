7145fd5d448ea01eeb9d4abddadda58cd0b57560
Devang Gandhi <117474877+Dev4057@users.noreply.github.com>
2026-02-12 14:13:15 +0530

    Update Snowflake session connection method

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,15 +1,13 @@
 # Import python packages
 import streamlit as st
-from snowflake.snowpark.context import get_active_session
 from snowflake.snowpark.functions import col
 # Get Snowflake session
-session = get_active_session()
+cnx=st.connection("snowflake")
+session = cnx.session()
 
 # Streamlit App UI
 st.title(":cup_with_straw: Customize Your Smoothie :cup_with_straw:")
 st.write("**Choose the fruits you want in your custom smoothie**")
-
-
 
 
 name_on_order = st.text_input("Name on smoothie")
